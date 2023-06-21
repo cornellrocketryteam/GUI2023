660d7d5fb30bd053e0fa60e6b5bf9e0d24b829e3
Nissi <97490148+nragland@users.noreply.github.com>
2023-06-20 20:19:32 -0600

    Update MapDisplay.py

--- MapDisplay.py
+++ MapDisplay.py
@@ -17,33 +17,45 @@ matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from Parsing import Parse
 
-class Map(Parse):
+class Map():
 
-    def __init__(self):
+    def __init__(self, gps, map):
         super().__init__()
         self.lat_min = 42.6740
         self.lat_max = 42.7320
         self.long_min = -77.2310
         self.long_max = -77.1521
         self.bounds = (self.long_min, self.long_max, self.lat_min, self.lat_max)
-        self.map_img = plt.imread('map.png')
+        self.map_img = plt.imread('compmap.png')
+        self.gps = gps
+        self.map = map
 
         # test launch coords
         #self.lat = 42.702980
         #self.long = -77.191559
 
+        # launch coords
+        #self.lat = 32.942863
+        #self.long = -106.91465
+
         # setup plot
         self.fig, self.ax = plt.subplots(figsize = (6,5))
-        self.ax.set_title('Test Launch Map')
+        self.ax.set_title('Launch Site')
         self.ax.set_xlim(self.bounds[0],self.bounds[1])
         self.ax.set_ylim(self.bounds[2],self.bounds[3])
 
         self.ax.imshow(self.map_img, zorder=0, extent = self.bounds, aspect= 'equal')
+        self.canvas = FigureCanvasTkAgg(self.fig, master = self.map)
+        self.canvas.get_tk_widget().pack(side = tk.TOP, fill = tk.BOTH, expand = True)
+        self.canvas.draw()
+
+        #self.ax.imshow(self.map_img, zorder=0, extent = self.bounds, aspect= 'equal')
         #im = plt.imread('map.png')
         #implot = plt.imshow(im, extent=self.bounds)
-        plt.show(block=False)
+        #plt.show(block=False)
 
-    def updateMap(self):
+    def updateMap(self, gps):
+        self.gps = gps
         self.ax.scatter(self.gps[1], self.gps[0], zorder=1, alpha= 0.2, c='b', s=10)
         self.ax.imshow(self.map_img, zorder=0, extent = self.bounds, aspect= 'equal')
-        plt.show(block=False)
+        self.canvas.draw()
