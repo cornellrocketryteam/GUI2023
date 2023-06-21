b5b2658f6534c0c868b59ad3900462360415973d
Nissi <97490148+nragland@users.noreply.github.com>
2023-06-20 20:20:13 -0600

    Update Parsing.py

--- Parsing.py
+++ Parsing.py
@@ -18,29 +18,35 @@ class Parse():
         self.jstr = ""
         self.gps = [0.0, 0.0]
         self.alt = 0.0
-        self.accel = 0.0
-        self.gyro = 0.0
-        self.mag = 0.0
+        self.accel = [0.0, 0.0, 0.0]
+        self.gyro = [0.0, 0.0, 0.0]
+        self.mag = [0.0, 0.0, 0.0]
         self.temp = 0.0
         self.message = ""
-        self.f = open('data.csv', 'a')
+        self.sensorreadings = [self.gps, self.alt, self.accel, self.gyro, self.mag, self.temp, self.message]
 
     def setValues(self):
+        """
+        Takes the current JSON string and parses it into each of its sensor components, then assigns these sensor values
+        to their corresponding attribute.
+        """
         # make into dictionary
         self.jstr = json.loads(self.jstr)
 
-        self.gps = [self.jstr["latitude"], self.jstr["longitude"]]
-        self.alt = self.jstr["elevation"]
-        self.accel = [self.jstr["accel_x"], self.jstr["accel_y"], self.jstr["accel_z"]]
-        self.gyro = [self.jstr["gyro_x"], self.jstr["gyro_y"], self.jstr["gyro_z"]]
-        self.mag = [self.jstr["mag_x"], self.jstr["mag_y"], self.jstr["mag_z"]]
-        self.temp = self.jstr["temp"]
-        self.message = self.jstr["messages"]
+        # self.gps = [self.jstr["latitude"], self.jstr["longitude"]]
+        self.alt = self.jstr["altitude_rel"] #self.jstr["elevation"]
+        # self.accel = [self.jstr["accel_x"], self.jstr["accel_y"], self.jstr["accel_z"]]
+        # self.gyro = [self.jstr["gyro_x"], self.jstr["gyro_y"], self.jstr["gyro_z"]]
+        # self.mag = [self.jstr["mag_x"], self.jstr["mag_y"], self.jstr["mag_z"]]
+        # self.temp = self.jstr["temp"]
+        # self.message = self.jstr["messages"]
+
+        self.sensorreadings = [self.gps, self.alt, self.accel, self.gyro, self.mag, self.temp, self.message]
 
     # for testing purposes
     def printValues(self):
-        print(self.jstr)
 
+        print(self.jstr)
         """
         print(self.gps)
         print(self.alt)
@@ -52,15 +58,20 @@ class Parse():
         """
 
     def serUpdate(self):
-        #ser = serial.Serial(port = "COM3", baudrate = 9600)
+        """
+        Reads from serial input and updates the attribute that holds this reading, then attempts
+        to update each sensor attribute if the reading is in the expected format.
+        """
         self.ser.reset_output_buffer()
         self.jstr = self.ser.readline().decode("utf-8")
+        self.f = open('data.csv', 'a')
         self.f.write(self.jstr)
         try:
             self.setValues()
+            self.printValues()
         except:
             pass
-        self.printValues()
+        self.f.close()
 
 
 #p = Parse()
@@ -89,4 +100,10 @@ JSON format reference:
    ]
 }
 
+"""
+
+
+"""
+{"callsign": "KD2ZGA", "altitude_abs": 1207, "altitude_rel": 1, "sd_init": 1,
+ "radio_init": 1, "altitude_armed": 1, "keyswitch_armed": 0, "id", 21932}
 """
